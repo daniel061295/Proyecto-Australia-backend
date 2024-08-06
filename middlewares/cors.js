e42edd90e5cc4489a83d71535fb55120cdcb4f83
Daniel Cardenas <daniel061295@gmail.com>
2024-08-06 11:37:03 -0500

    Agregando opcion para aceptar todos los origenes

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,12 +1,13 @@
 import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:8080'
+  '*'
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
-    if (acceptedOrigins.includes(origin)) {
+    console.log(origin);
+    if (acceptedOrigins.includes(origin) || acceptedOrigins.includes('*')) {
       return callback(null, true);
     }
 
